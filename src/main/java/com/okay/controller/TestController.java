0457aac5cb9b567594ed261884a538964d4c6c31
Ahmet Okay <ahmetokay@users.noreply.github.com>
2021-02-07 05:45:23 +0300

    Add files via upload

--- src/main/java/com/okay/controller/TestController.java
+++ src/main/java/com/okay/controller/TestController.java
@@ -3,7 +3,6 @@ package com.okay.controller;
 import com.okay.model.User;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.data.JRJpaDataSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,13 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class TestController {
+
+    private final DataSource dataSource;
+
+    public TestController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @ResponseBody
     @GetMapping(value = "/test")
@@ -39,6 +46,29 @@ public class TestController {
             ServletOutputStream outputStream = response.getOutputStream();
             JasperExportManager.exportReportToPdfStream(print, outputStream);
         } catch (IOException | JRException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @ResponseBody
+    @GetMapping(value = "/db")
+    public void db(HttpServletResponse response, @RequestParam(value = "nameQuery", required = false) String nameQuery, @RequestParam(value = "surnameQuery", required = false) String surnameQuery) {
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/jasper/db.jrxml");
+
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("nameQuery", nameQuery == null ? "" : nameQuery);
+            parameters.put("surnameQuery", surnameQuery == null ? "" : surnameQuery);
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+            JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, dataSource.getConnection());
+
+            ServletOutputStream outputStream = response.getOutputStream();
+            JasperExportManager.exportReportToPdfStream(print, outputStream);
+        } catch (IOException | JRException | SQLException e) {
             e.printStackTrace();
         }
     }
